383a963b6a917c2842e58ff9fcd5ae29b371b208
Jaysolanki7 <js9302244036@gmail.com>
2025-05-12 13:46:40 +0530

    update

---
+++ story_1.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std;
+int main(){
+    
+    int n;
+    cout<<"Enter number of days : ";
+    cin>>n;
+
+    int restingdays =0;
+    int starroots =0;
+    int sunbuds=0;
+    int crystalflowers =0;
+    int moonblossoms =0;
+    int wildleafs =0;
+    int totalflowers =0;
+
+    for(int i=1 ;i<=n;i++){
+        if(i%2==0 && i%3 ==0){
+            crystalflowers++;
+        }
+        else if(i%2==0 && i%3 ==0 && i%4 ==0){
+            restingdays++;
+        }
+        else if(i%2==0){
+            sunbuds+=2;
+        }
+        else if(i%3 ==0){
+            moonblossoms+=3;
+        }
+        else if(i%4 ==0){
+            starroots+=4;
+        }
+        else if(i%2==0 && i%3 ==0 && i%4 ==0){
+            restingdays++;
+        }
+        else {
+            wildleafs++;
+        }
+
+    }
+    totalflowers=(starroots+sunbuds+crystalflowers+moonblossoms+wildleafs);
+    cout << "\n**********Results**********\n";
+    cout << "Sunbuds: " << sunbuds << "\n";
+    cout << "Moonblossoms: " << moonblossoms << "\n";
+    cout << "Starroots: " << starroots << "\n";
+    cout << "Crystal Flowers: " << crystalflowers << "\n";
+    cout << "Wildleafs: " << wildleafs << "\n";
+    cout << "Soil Resting Days: " << restingdays << "\n";
+    cout << "Total Flowers Planted: " << totalflowers << "\n";
+
+    return 0;
+}
---
+++ story_2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int F;
+    cout << "Enter top floor (F): ";
+    cin >> F;
+
+    int totalRings = 0;
+    int totalWaits = 0;
+    int totalTime = 0;
+
+    for (int floor = 1; floor <= F; floor++) {
+        if (floor % 2 != 1) {
+            totalRings += 1;
+        } 
+        else if (floor % 4 == 0) {
+            totalRings += 2;
+        }
+
+        if (floor % 7 == 0 && !(floor % 4 == 0 && floor % 7 == 0)) {
+            totalWaits += 1;
+        }
+
+        totalTime += 2;
+    }
+
+    totalTime += totalWaits * 5;
+
+    cout << "Total rings: " << totalRings << endl;
+    cout << "Total waits: " << totalWaits << endl;
+    cout << "Total time: " << totalTime << " seconds" << endl;
+
+    return 0;
+}
